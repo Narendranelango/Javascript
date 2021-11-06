2d517bf759e7d6140539634848a31ef88ed2c367
Narendran Elango <narendran275@gmail.com>
2021-11-07 00:44:49 +0530

    2:In Built Function's in String

--- Javascript - I/Practice.js
+++ Javascript - I/Practice.js
@@ -4,3 +4,16 @@ var str1="I love \"Java Script\" "
 var str2='I love \'Java Script\' '
 console.log(str1)
 console.log(str2)
+
+                                                /*In Built String Function's */
+var naren="Hi Narendran!"
+console.log(naren.length)
+console.log(naren.slice(1,7))
+console.log(naren.substr(4,5))
+console.log(naren.toUpperCase())
+console.log(naren.toLowerCase())
+var narendran="Hi, How, Are, you"
+var na="      Na"
+console.log(na.trim())
+console.log(narendran.split(','))
+console.log(narendran.charAt(1))
