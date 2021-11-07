52c5af29c3acdb28a905e149e9fc823cf4d67a19
Narendran Elango <narendran275@gmail.com>
2021-11-07 19:00:34 +0530

    5: Type Coersion

--- Javascript - I/Practice.js
+++ Javascript - I/Practice.js
@@ -42,3 +42,33 @@ if(4 === '4'){
 else{
     console.log(false)
 }
+
+                                /*Type Coercion */
+//Implicit
+var a='5'
+var b="Neren"
+var c=a+b
+console.log(typeof(c))
+console.log(typeof(a+true))
+console.log(typeof(2>=a))
+console.log(typeof(2>=a))
+
+//Explicit
+var a= 54
+console.log(typeof(a));
+console.log(typeof(String(a)))
+console.log(typeof(String(true)))
+console.log(typeof(String(null)))
+console.log(typeof(String(undefined)))
+
+var n="Narendran"
+console.log(typeof(n));
+console.log(typeof(Number(n)))
+console.log(typeof(Number(true)))
+console.log(typeof(Number('54')))
+console.log(typeof(Number(5.11)))
+
+console.log(Boolean(0),typeof(Boolean(0)))
+console.log(Boolean(1),typeof(Boolean(1)))
+console.log(Boolean('Naren'),typeof(Boolean('Naren')))
+console.log(Boolean(' '),typeof(Boolean(' ')))
