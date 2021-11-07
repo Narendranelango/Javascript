4c2f21d2147b9ed39732ced13fc2bcfc6a06db8e
Narendran Elango <narendran275@gmail.com>
2021-11-07 21:27:27 +0530

    6: Arrays

--- Javascript - I/objectsAndArrays.js
+++ Javascript - I/objectsAndArrays.js
@@ -1,4 +1,4 @@
-// console.clear()
+console.clear()
 var car={
     brand:"Volkswagen",
     model:"Polo",
@@ -29,3 +29,37 @@ var bike={
 console.log("Brand: "+car.brand+" Model:"+car.model)
 console.log(car.owner)
 console.log(bike.fun())
+
+
+//Arrays-I
+var mFriends=["Viki","Manish","Pranav","Aakash"]
+console.log(mFriends)
+console.log(typeof(mFriends))
+console.log(mFriends[0])
+console.log(mFriends[3])
+mFriends[3]="JP"
+console.log(mFriends[3])
+mFriends.push("Senthil", "Narayanan")
+console.log(mFriends)
+mFriends.pop(mFriends)
+console.log(mFriends)
+
+
+//Arrays-II
+//To add
+
+//first parameter - Tells where to start.
+//second parameter - How many items to be deleted.
+//third parameter - What to add.
+mFriends.splice(1,0,'Maneesh','Jai Prakash')
+console.log(mFriends)
+
+//To delete
+mFriends.splice(1,1)
+console.log(mFriends)
+
+var schoolFriends=["Dinesh","Chandrasekar","Ajith","Mohan","Bharath"]
+var friends=schoolFriends.concat(mFriends)
+console.log(friends)
+console.log(friends.sort())
+console.log(friends.reverse())
