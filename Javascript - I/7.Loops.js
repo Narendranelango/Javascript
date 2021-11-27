598a2144ab4eb38af67c4001eb0f04b9df63af11
Narendran Elango <narendran275@gmail.com>
2021-11-27 19:39:55 +0530

    7: Loops

---
+++ Javascript - I/7.Loops.js
@@ -0,0 +1,14 @@
+console.clear()
+//While
+var mVal=0
+while(mVal<50){
+    mVal++
+}
+console.log(mVal)
+//For
+var list=[1,2,3,4]
+var i;
+for(i=0;i<list.length;i++)
+    {
+        console.log("Pos : "+i+" Val : "+list[i]);
+    }
