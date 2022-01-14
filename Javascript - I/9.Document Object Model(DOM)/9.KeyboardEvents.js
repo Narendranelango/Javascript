a86b537fa69291d354eba822a65fbded8fddc46d
Narendran Elango <narendran275@gmail.com>
2022-01-14 17:27:57 +0530

    9: Keyboard Events

---
+++ Javascript - I/9.Document Object Model(DOM)/9.KeyboardEvents.js
@@ -0,0 +1,11 @@
+document.body.addEventListener('keyup',function(ob){//We use 'ob' get the value in the main-id
+  console.log("Key Up")
+})
+
+document.body.addEventListener('keydown',function(ob){//We use 'ob' get the value in the main-id
+    console.log("Key Down")
+  })
+
+document.body.addEventListener('keypress',function(ob){//We use 'ob' get the value in the main-id
+    console.log("Key Press")
+  })
