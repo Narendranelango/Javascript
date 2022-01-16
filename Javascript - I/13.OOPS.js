78ab1347baab7a063508ffef6f8dbe273f0f6686
Narendran Elango <narendran275@gmail.com>
2022-01-16 16:56:33 +0530

    Update 13.OOPS.js

--- Javascript - I/13.OOPS.js
+++ Javascript - I/13.OOPS.js
@@ -7,5 +7,19 @@ var stu_Det = {
         return this.edu
     }
 }
-console.log(stu_Det.edu)
-console.log(stu_Det.getEdu)
+stu_Det.edu
+stu_Det.getEdu()
+
+// Use of this keyword
+var edu="BE CSE"
+var stu_Det = {
+    name : "Naren",
+    age  : 22,
+    edu  : "Bachelor Of Computer Science And Engineering",
+    per  : 74,
+    getEdu : function() {
+        console.log(this.edu)//Points to stu_edu property.
+        console.log(edu)//points to global var edu.
+    }
+}
+stu_Det.getEdu()
