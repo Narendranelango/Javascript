7542fb0aea2821f086b08d4b7b0339b0aef063be
Narendran Elango <narendran275@gmail.com>
2022-01-16 16:43:15 +0530

    13 : OOPS : Intro

---
+++ Javascript - I/13.OOPS.js
@@ -0,0 +1,11 @@
+var stu_Det = {
+    name : "Naren",
+    age  : 22,
+    edu  : "Bachelor Of Computer Science And Engineering",
+    per  : 74,
+    getEdu : function() {
+        return this.edu
+    }
+}
+console.log(stu_Det.edu)
+console.log(stu_Det.getEdu)
