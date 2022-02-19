1be4be48cfce266984477a86f9fe30e2449f11b3
Narendran Elango <narendran275@gmail.com>
2022-02-20 02:26:10 +0530

    Update 14.IntroTOAJAX.js

--- Javascript - I/14.IntroTOAJAX.js
+++ Javascript - I/14.IntroTOAJAX.js
@@ -4,7 +4,10 @@ function getData() {
         if(this.readyState === 4) {
             if(this.status == 200) {
             console.log("Response Generated")
-            console.log(JSON.parse(this.responseText))
+            var res = JSON.parse(this.responseText)
+                for(var i =0; i<res.length; i++)
+                    console.log(res[i].id, res[i].title)
+
             }
             else {
                 console.log("Call Failed :(")
